cfc74e115bacc031e8013e1031c2db5f000d5158
Pawel Godula <bolomaster7@gmail.com>
2022-12-05 22:52:07 +0100

    start working with redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,9 @@ import styles from './Column.module.scss'
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm'
 
+
 const Column = props => {
+
   return (
     <article className={styles.column}>
       <h2 className={styles.title}>
