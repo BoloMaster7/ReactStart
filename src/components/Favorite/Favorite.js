761e57531c9aa5f7f58224e4f6cce02f47e400f0
Pawel Godula <bolomaster7@gmail.com>
2022-12-11 20:35:11 +0100

    add reactRouter

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,15 @@
+//import styles from './Favorite.module.scss'
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = ()  => {
+  return (
+    <div>
+      <Container>
+      <PageTitle>Favourite</PageTitle>
+        <p>Lorem ipsum</p>
+      </Container>
+    </div>
+  );
+};
+export default Favorite;
