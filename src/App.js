2b592ad494b3c188224db3ef78c3635272f7330a
Pawel Godula <bolomaster7@gmail.com>
2022-12-11 22:23:02 +0100

    add page not found, not working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import Container from './components/Container/Container'
+//import Container from './components/Container/Container'
 import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
 import About from './components/About/About';
 import Favorite from './components/Favorite/Favorite';
-import { Routes, Route } from 'react-router-dom';
+import NotFound from './components/NotFound/NotFound';
+import { Routes, Route} from 'react-router-dom';
 
 
 const App = () => {
   return (
 <main>
-      <NavBar />
-      <Container>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/favorite" element={<Favorite />} />
-        </Routes>
-      </Container>
+      <NavBar />     
+<Routes>
+   <Route path="/" component={Home} />
+   <Route path="/about" component={About} />
+   <Route path="/favorite" component={Favorite} />
+   <Route path="*" component={NotFound} />
+</Routes>
  </main>
   );
 };
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+//import styles from './NotFound.module.scss'
+import Container from '../Container/Container';
+import PageTitle from '../PageTitle/PageTitle';
+
+const NotFound = ()  => {
+  return (
+    <div>
+      <Container>
+      <PageTitle>404 NOT FOUND </PageTitle>      
+      </Container>
+    </div>
+  );
+};
+export default NotFound;
