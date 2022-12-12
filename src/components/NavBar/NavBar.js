ba7b84d36e685ddee93c02384b081b018a13baed
Pawel Godula <bolomaster7@gmail.com>
2022-12-12 22:47:02 +0100

    add getListById

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
 import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -10,11 +11,19 @@ const NavBar = () => {
        <Container>
         <div className={styles.navigation}>
         <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"><Link to="/" /></ i>
-         
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/favorite">Favorite</Link></li>
-                <li><Link to="/about">About</Link></li>
+            <li><NavLink className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined}
+              to="/">Home
+                </NavLink></li>
+                <li><NavLink className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined}
+              to="/favorite">Favorite
+                    </NavLink></li>
+                <li><NavLink className={({ isActive }) =>
+                isActive ? styles.linkActive : undefined}
+              to="/about">About
+                    </NavLink></li>
             </ul>
             </div>
         </Container>
